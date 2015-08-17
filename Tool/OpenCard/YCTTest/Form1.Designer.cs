56881ae3eb268943d28cb4bae10f733f8a77b52b
233955501 <233955501@qq.com>
2015-08-17 21:27:10 +0800

    1

--- Tool/OpenCard/YCTTest/Form1.Designer.cs
+++ Tool/OpenCard/YCTTest/Form1.Designer.cs
@@ -307,6 +307,7 @@
             this.button1.TabIndex = 30;
             this.button1.Text = "测试命令";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label10
